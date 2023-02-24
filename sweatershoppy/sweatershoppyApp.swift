1d0b3a3b81716bdf5a35040a010a78e8b1ab8aa4
Aryan Jagarwal <ajbaggar@gmail.com>
2023-02-24 19:55:19 +0530

    Initial Commit

---
+++ sweatershoppy/sweatershoppyApp.swift
@@ -0,0 +1,17 @@
+//
+//  sweatershoppyApp.swift
+//  sweatershoppy
+//
+//  Created by Aryan Jagarwal on 24/02/23.
+//
+
+import SwiftUI
+
+@main
+struct sweatershoppyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
