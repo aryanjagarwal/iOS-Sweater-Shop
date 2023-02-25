c46e198493e2b63c66ac80c465e4231b1672af98
Aryan Jagarwal <ajbaggar@gmail.com>
2023-02-25 13:07:03 +0530

    cartView update

--- sweatershoppy/Components/ProductCard.swift
+++ sweatershoppy/Components/ProductCard.swift
@@ -8,6 +8,7 @@
 import SwiftUI
 
 struct ProductCard: View {
+    @EnvironmentObject var cartManager: CartManager
     var product: Product
     
     var body: some View {
@@ -35,7 +36,7 @@ struct ProductCard: View {
             .shadow(radius: 3)
             
             Button {
-                print("Added to cart")
+                cartManager.addToCart(product: product)
             }label: {
                 Image(systemName: "plus")
                     .padding(10)
@@ -52,5 +53,6 @@ struct ProductCard: View {
 struct ProductCard_Previews: PreviewProvider {
     static var previews: some View {
         ProductCard(product: productList[0])
+            .environmentObject(CartManager())
     }
 }
